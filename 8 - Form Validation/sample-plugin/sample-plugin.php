9716da5cbd8094d5a1cc095fb938cf5193503781
Alexey Utkin <31668514+SuperAppps@users.noreply.github.com>
2017-11-21 12:06:38 +0300

    Update template header

--- 8 - Form Validation/sample-plugin/sample-plugin.php
+++ 8 - Form Validation/sample-plugin/sample-plugin.php
@@ -1,15 +1,15 @@
 <?php
 	
 /*
-Plugin Name: Snappy List Builder
-Plugin URI: http://wordpressplugincourse.com/plugins/snappy-list-builder
-Description: The ultimate email list building plugin for WordPress. Capture new subscribers. Reward subscribers with a custom download upon opt-in. Build unlimited lists. Import and export subscribers easily with .csv
-Version: 1.0
-Author: Joel Funk @ Code College
-Author URI: http://joelfunk.codecollege.ca
+Plugin Name: <Sample Plugin>
+Plugin URI: <https://portfolio.appcloud.su/plugins/sample-plugin>
+Description: <The ultimate ... plugin for WordPress. Capture ... Reward ... Build ... Import and export ... easily with .csv>
+Version: <1.0>
+Author: <Alexey Utkin @ SuperAppps>
+Author URI: <https://portfolio.appcloud.su/>
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: snappy-list-builder
+Text Domain: <sample-plugin>
 */
 
 
